11f0a3422c9b246ba1b4a62cb15a75d072b53e9f
marchite <gilangaryadimahardika@gmail.com>
2022-02-12 18:39:39 +0700

    gilang ngetes

--- application/views/dashboard_v.php
+++ application/views/dashboard_v.php
@@ -1,3 +1,4 @@
+[
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
 	<!-- Content Header (Page header) -->
@@ -376,7 +377,8 @@
 															Count Dracula
 															<small class="contacts-list-date float-right">2/28/2015</small>
 														</span>
-														<span class="contacts-list-msg">How have you been? I was...</span>
+														<span class="contacts-list-msg">How have you been? I
+															was...</span>
 													</div>
 													<!-- /.contacts-list-info -->
 												</a>
@@ -581,59 +583,73 @@
 									</thead>
 									<tbody>
 										<tr>
-											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR9842</a></td>
+											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR9842</a>
+											</td>
 											<td>Call of Duty IV</td>
 											<td><span class="badge badge-success">Shipped</span></td>
 											<td>
-												<div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+												<div class="sparkbar" data-color="#00a65a" data-height="20">
+													90,80,90,-70,61,-83,63</div>
 											</td>
 										</tr>
 										<tr>
-											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR1848</a></td>
+											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR1848</a>
+											</td>
 											<td>Samsung Smart TV</td>
 											<td><span class="badge badge-warning">Pending</span></td>
 											<td>
-												<div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
+												<div class="sparkbar" data-color="#f39c12" data-height="20">
+													90,80,-90,70,61,-83,68</div>
 											</td>
 										</tr>
 										<tr>
-											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR7429</a></td>
+											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR7429</a>
+											</td>
 											<td>iPhone 6 Plus</td>
 											<td><span class="badge badge-danger">Delivered</span></td>
 											<td>
-												<div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
+												<div class="sparkbar" data-color="#f56954" data-height="20">
+													90,-80,90,70,-61,83,63</div>
 											</td>
 										</tr>
 										<tr>
-											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR7429</a></td>
+											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR7429</a>
+											</td>
 											<td>Samsung Smart TV</td>
 											<td><span class="badge badge-info">Processing</span></td>
 											<td>
-												<div class="sparkbar" data-color="#00c0ef" data-height="20">90,80,-90,70,-61,83,63</div>
+												<div class="sparkbar" data-color="#00c0ef" data-height="20">
+													90,80,-90,70,-61,83,63</div>
 											</td>
 										</tr>
 										<tr>
-											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR1848</a></td>
+											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR1848</a>
+											</td>
 											<td>Samsung Smart TV</td>
 											<td><span class="badge badge-warning">Pending</span></td>
 											<td>
-												<div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
+												<div class="sparkbar" data-color="#f39c12" data-height="20">
+													90,80,-90,70,61,-83,68</div>
 											</td>
 										</tr>
 										<tr>
-											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR7429</a></td>
+											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR7429</a>
+											</td>
 											<td>iPhone 6 Plus</td>
 											<td><span class="badge badge-danger">Delivered</span></td>
 											<td>
-												<div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
+												<div class="sparkbar" data-color="#f56954" data-height="20">
+													90,-80,90,70,-61,83,63</div>
 											</td>
 										</tr>
 										<tr>
-											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR9842</a></td>
+											<td><a href="<?= base_url('assets/'); ?>pages/examples/invoice.html">OR9842</a>
+											</td>
 											<td>Call of Duty IV</td>
 											<td><span class="badge badge-success">Shipped</span></td>
 											<td>
-												<div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+												<div class="sparkbar" data-color="#00a65a" data-height="20">
+													90,80,90,-70,61,-83,63</div>
 											</td>
 										</tr>
 									</tbody>
@@ -644,7 +660,8 @@
 						<!-- /.card-body -->
 						<div class="card-footer clearfix">
 							<a href="javascript:void(0)" class="btn btn-sm btn-info float-left">Place New Order</a>
-							<a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All Orders</a>
+							<a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All
+								Orders</a>
 						</div>
 						<!-- /.card-footer -->
 					</div>
@@ -855,4 +872,4 @@
 	</section>
 	<!-- /.content -->
 </div>
-<!-- /.content-wrapper -->
+<!-- /.content-wrapper -->]
